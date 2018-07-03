5ca593390881e0911271995c36dbf013c06e43c1
meri <mariachiara.zaccardi@mail.polimi.it>
2018-07-03 14:51:47 +0200

    modify tokenization

--- dlp/tokenizer.py
+++ dlp/tokenizer.py
@@ -21,7 +21,7 @@ class TokenizerPosts(object):
     def __init__(self, df):
         #super(Tokenizer, self).__init__()
         self.soups=list(df["Text"])
-
+        #self.soups = df
     
 #    def __replace_links(self, soup):
 #        """
@@ -39,12 +39,22 @@ class TokenizerPosts(object):
         code=re.compile(util.CODE)
         return re.sub(code, 'thistokeniscode', soup)
     
+    def __deletetabulation(self, soup):
+        """
+        Delete any tabulation as \n, \t, .. 
+        """
+        tab = re.compile(util.TAB)
+        return re.sub(tab, '', soup)
+    
     def __deletenochar(self, soup):
         """
-        Delete any no char as \n, \t, .. 
+        Delete any no char as £,$,%,..
         """
-        nochar = re.compile(util.NOCHAR)
-        return re.sub(nochar, '', soup)
+        nochar= re.compile(util.NOCHAR)
+        if nochar.fullmatch(soup)==None:
+            #soup=re.sub(nochar, '', soup)        
+            return soup
+    
 
 
     def __replace_time(self, soup_text):
@@ -52,7 +62,9 @@ class TokenizerPosts(object):
         Replace the time format in the text of a single soup with a unique string
         """
         time=re.compile(util.TIME)
-        return re.sub(time, 'thistokenistime', soup_text)
+        if time.fullmatch(soup_text)!=None:
+            soup_text=re.sub(time, 'thistokenistime', soup_text)        
+        return soup_text
     
     
     def __replace_date(self, soup_text):
@@ -60,7 +72,9 @@ class TokenizerPosts(object):
         Replace the date format in the text of a single soup with a unique string
         """
         date=re.compile(util.DATE)
-        return re.sub(date, 'thistokenisdate', soup_text)
+        if date.fullmatch(soup_text)!=None:
+            soup_text=re.sub(date, 'thistokenisdate', soup_text)        
+        return soup_text
     
     
     def __replace_version(self, soup_text):
@@ -68,7 +82,10 @@ class TokenizerPosts(object):
         Replace the version format in the text of a single soup with a unique string
         """
         version=re.compile(util.VERSION)
-        return re.sub(version, 'thistokenisversion', soup_text)
+        #return re.sub(version, 'thistokenisversion', soup_text)
+        if version.fullmatch(soup_text)!=None:
+            soup_text=re.sub(version, 'thistokenisversion', soup_text)        
+        return soup_text
     
     
     def __replace_path(self, soup_text):
@@ -76,15 +93,19 @@ class TokenizerPosts(object):
         Replace the paths format in the text of a single soup with a unique string
         """
         path=re.compile(util.PATH)
-        return re.sub(path, 'thistokenispath', soup_text)
+        if path.fullmatch(soup_text)!=None:
+            soup_text=re.sub(path, 'thistokenispath', soup_text)        
+        return soup_text
     
     
     def __replace_hexadecimal(self, soup_text):
         """
         Replace the hexadecimal format in the text of a single soup with a unique string
         """
-        hexadeciaml=re.compile(util.HEXADECIMAL)
-        return re.sub(hexadeciaml, 'thistokenishexadecimal', soup_text)
+        hexadecimal=re.compile(util.HEXADECIMAL)
+        if hexadecimal.fullmatch(soup_text)!=None:
+            soup_text= re.sub(hexadecimal, 'thistokenishexadecimal', soup_text)        
+        return soup_text
     
     
     def __replace_variable(self, soup_text):
@@ -92,19 +113,39 @@ class TokenizerPosts(object):
         Replace the variables format in the text of a single soup with a unique string
         """
         variable=re.compile(util.VARIABLE)
-        return re.sub(variable, 'thistokenisvariable', soup_text)
+        if variable.fullmatch(soup_text)!=None:
+            soup_text=re.sub(variable, 'thistokenisvariable', soup_text)        
+        return soup_text
     
     
-    def __filter_word(self, token):
+    def __filter_word(self, doc):
         
-        token=self.__replace_time(token)
-        token=self.__replace_date(token)
-        token=self.__replace_version(token)
-        token=self.__replace_path(token)
-        token=self.__replace_hexadecimal(token)
-        token=self.__replace_variable(token)
+        lemmas = []
+        for token in doc:
+            if not (token.is_punct or token.is_space or 
+                    token.dep_==('det' or 'aux' or 'poss' or 'prep' or 'nsubj' 
+                                 or 'nsubjpass' or 'dobj')):
+                if token.like_url:
+                    token.lemma_ = 'thistokenisurl'
+                token = token.lemma_
+                token_tmp = self.__deletenochar(token)
+                if token_tmp!=None:
+                    token=self.__replace_time(token)
+                    token=self.__replace_date(token)
+                    token=self.__replace_version(token)
+                    token=self.__replace_path(token)
+                    token=self.__replace_hexadecimal(token)
+                    token=self.__replace_variable(token)
+                    lemmas.append(token)
+#            print (self.__deletenochar(token))
+#        if self.__deletenochar(token)==None:
+#            pass
+#        else:
+#            token=self.__deletenochar(token) 
+#            print('ELSE', token)
 
-        return token
+#        return token
+        return lemmas
     
     def __tokenize_single_soup(self, soup):
         """
@@ -112,7 +153,7 @@ class TokenizerPosts(object):
         """
         #self.__replace_links(soup)
         soup = self.__replace_code(soup)
-        soup = self.__deletenochar(soup)
+        soup = self.__deletetabulation(soup)
         root= etree.fromstring(soup, etree.HTMLParser())
         soup_text=(' '.join(root.xpath("//text()")))
 #        nlp= spacy.load("en")
@@ -139,8 +180,13 @@ class TokenizerPosts(object):
         nlp.remove_pipe('tagger')
         documents = []
         for d in nlp.pipe(docs):
-            lemmas = [self.__filter_word(doc.lemma_) for doc in d if not (doc.is_punct or doc.dep_==('det' or 'aux' or 'poss' or 'prep' or 'nsubj' or 'nsubjpass' or 'dobj'))]
+            #lemmas = [self.__filter_word(d) for doc in d if not (doc.is_punct or doc.is_space or doc.dep_==('det' or 'aux' or 'poss' or 'prep' or 'nsubj' or 'nsubjpass' or 'dobj'))]
+            lemmas = self.__filter_word(d)
             documents.append(lemmas)
         
         return documents
+
+#posts_df = ['<p>https://127.0.0.1 ciao</p>a  www.ciao.com # & >  tutti /meri/Projects/deep-learning!']
+#tokenizer = TokenizerPosts(posts_df)
+#result = tokenizer.tokenize()
     
